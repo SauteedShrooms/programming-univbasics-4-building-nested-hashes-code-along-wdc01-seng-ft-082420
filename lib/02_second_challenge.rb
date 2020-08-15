5ecb3b0a7db150e6ee548dcb4cf6b37baf11ab61
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-15 19:24:37 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -9,8 +9,10 @@ def second_challenge
     :capulet => {
       :patriarch => {},
       :matriarch => {},
-      
-      }}
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
 
   
 end
